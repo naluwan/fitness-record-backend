1ce089e673369510b4cc7a79df45cc4f151980d7
naluwan <street.baller06@gmail.com>
2023-04-20 16:35:06 +0800

    feat: add record controller

--- controllers/record-controller.js
+++ controllers/record-controller.js
@@ -1,6 +1,14 @@
+const { recordServices } = require('../services');
 const recordController = {
   getRecords: (req, res, next) => {
-    res.send('健身紀錄');
+    recordServices.getRecords(req, (err, data) => {
+      err ? next(err) : res.json({ status: 'success', data });
+    });
+  },
+  postRecord: (req, res, next) => {
+    recordServices.postRecord(req, (err, data) => {
+      err ? next(err) : res.json({ status: 'success', data });
+    });
   },
 };
 
