f4cf3beb70f1d2844ff7d70aef9ef0bd201685ae
naluwan <street.baller06@gmail.com>
2023-05-17 20:10:20 +0800

    feat: update authenticated to delete sensitive data of user

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,12 @@ const passport = require('../config/passport');
 
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user)
+    if (err || !user) {
       return res.status(401).json({ status: 'error', message: '請先登入再進行操作' });
-    req.user = user;
+    }
+    const userData = user.toJSON();
+    delete userData.password;
+    req.user = userData;
     next();
   })(req, res, next);
 };
