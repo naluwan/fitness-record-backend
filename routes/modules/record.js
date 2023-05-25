fb3ba990685f7f35bd8098cfece650f5e501adf7
naluwan <street.baller06@gmail.com>
2023-05-25 18:01:28 +0800

    feat: update post record router for upload multiple images

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const recordController = require('../../controllers/record-controller');
 const { authenticated } = require('../../middleware/auth');
+const upload = require('../../middleware/multer');
 
 router.get('/:id/edit', authenticated, recordController.editRecord);
 router.put('/:id', authenticated, recordController.putRecord);
 router.delete('/:id', authenticated, recordController.deleteRecord);
-router.post('/', authenticated, recordController.postRecord);
+router.post('/', authenticated, upload.array('images'), recordController.postRecord);
 router.get('/', recordController.getRecords);
 
 module.exports = router;
