8749c3c6680b99af36652a1c0caef9d72e17de8f
naluwan <street.baller06@gmail.com>
2023-05-25 17:58:19 +0800

    feat: add image model

---
+++ migrations/20230525063808-create-image.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      record_id: {
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      url: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Images');
+  },
+};
---
+++ models/image.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Image.belongsTo(models.Record, { foreignKey: 'recordId' });
+      Image.belongsTo(models.User, { foreignKey: 'userId' });
+    }
+  }
+  Image.init(
+    {
+      recordId: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
+      url: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Image',
+      tableName: 'Images',
+      underscored: true,
+    },
+  );
+  return Image;
+};
