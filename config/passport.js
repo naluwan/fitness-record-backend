a8ad6e0e40a63b622c24b36f780a6b939cbd053b
naluwan <street.baller06@gmail.com>
2023-05-29 16:22:24 +0800

    feat: add line login strategy

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,10 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const LineStrategy = require('passport-line-auth').Strategy;
 const passportJWT = require('passport-jwt');
 const bcrypt = require('bcryptjs');
 const { User } = require('../models');
+const jwt = require('jsonwebtoken');
 
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
@@ -34,6 +36,46 @@ passport.use(
       .then((user) => cb(null, user))
       .catch((err) => cb(err));
   }),
+);
+
+passport.use(
+  new LineStrategy(
+    {
+      channelID: process.env.LINE_LOGIN_CHANNEL_ID,
+      channelSecret: process.env.LINE_LOGIN_CHANNEL_SECRET,
+      callbackURL: process.env.LINE_LOGIN_CALLBACK_URL,
+      scope: ['profile', 'openid', 'email'],
+    },
+    (accessToken, refreshToken, params, profile, cb) => {
+      // 添加email資料
+      const { email } = jwt.decode(params.id_token);
+      profile.email = email;
+
+      // 確認使用者
+      return User.findOne({ where: { email: profile.email } }).then((user) => {
+        // 有該使用者資料就返回
+        if (user) return cb(null, user);
+
+        // 沒有資料就建立一個，密碼採隨機產生
+        const randomPassword = Math.random().toString(36).slice(-8);
+        bcrypt
+          .genSalt(10)
+          .then((salt) => bcrypt.hash(randomPassword, salt))
+          .then((hash) =>
+            User.create({
+              name: profile.displayName,
+              email,
+              weight: null,
+              waistline: null,
+              password: hash,
+              avatar: profile.pictureUrl || 'https://i.imgur.com/PGbAlS3.png',
+            }),
+          )
+          .then((user) => cb(null, user))
+          .catch((err) => cb(err));
+      });
+    },
+  ),
 );
 
 // 序列化 - 將資料轉為可儲存形式的過程(process)
