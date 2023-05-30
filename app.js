b67f6354964c5c1062f7a418203cf522cda63765
naluwan <street.baller06@gmail.com>
2023-05-30 16:11:46 +0800

    feat: add white list

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
     credentials: true,
     preflightContinue: false,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    origin: '*',
+    origin: process.env.WHITELIST,
   }),
 );
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
