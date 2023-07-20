4dae7206215ae8199b482ab78402e696592288ce
naluwan <street.baller06@gmail.com>
2023-07-20 19:26:11 +0800

    feat: add pagination-helpers

---
+++ helpers/pagination-helper.js
@@ -0,0 +1,19 @@
+const getOffset = (limit = 10, page = 1) => (page - 1) * limit;
+const getPagination = (limit = 10, page = 1, total = 50) => {
+  const totalPage = Math.ceil(total / limit);
+  const pages = Array.from({ length: totalPage }, (_, index) => index + 1);
+  const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page;
+  const hasPrevPage = currentPage - 1 > 1;
+  const hasNextPage = currentPage + 1 < totalPage;
+  return {
+    pages,
+    totalPage,
+    currentPage,
+    hasPrevPage,
+    hasNextPage,
+  };
+};
+module.exports = {
+  getOffset,
+  getPagination,
+};
