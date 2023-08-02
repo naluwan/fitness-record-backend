57217221e6dd83ca822a8a757cddd3c83d91846d
naluwan <street.baller06@gmail.com>
2023-08-02 17:11:41 +0800

    feat: add socket.io

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('./config/passport');
 const cors = require('cors');
+const socket = require('socket.io');
+const http = require('http');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// 實作socket.io
+const server = http.Server(app);
+const io = socket(server, { cors: { origin: '*' } });
 
 app.use(express.json());
 app.use(
@@ -27,7 +33,23 @@ app.use(passport.session());
 app.use(flash());
 app.use(routes);
 
-app.listen(port, () => {
+// socket.io監聽事件
+io.on('connection', (socket) => {
+  console.log('================ socket connect!! ==================');
+  console.log(`socket ID ====> `, socket.id);
+
+  socket.on('updateRanking', (needUpdateRanking) => {
+    console.log('from client socket ====>', needUpdateRanking);
+    io.sockets.emit('updateRanking', needUpdateRanking);
+  });
+  socket.on('disconnect', () => {
+    console.log('disconnect');
+    io.sockets.emit('disConnect', 'user close page');
+  });
+});
+
+// 這邊要改成直接監聽socket server
+server.listen(port, () => {
   console.info(`fitnessRecord app listening on http://localhost:${port}`);
 });
 
